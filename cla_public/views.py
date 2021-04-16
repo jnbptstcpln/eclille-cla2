7f0ca8fbbc3f62b93dfe40c9e52fa3c4efeee0c0
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-16 16:06:04 +0200

    Nettoyage des views

--- cla_public/views.py
+++ cla_public/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
 
-from cla_bdx.models import Campaign
-
 
 class AbstractPublicView(View):
 
@@ -14,14 +12,11 @@ class AbstractPublicView(View):
 class IndexPublicView(AbstractPublicView):
 
     def get(self, req):
-        on_going_campaign = Campaign.objects.ongoing()
-        on_going_campaign_vote = Campaign.objects.vote_ongoing()
 
         return render(
             req,
             "cla_public/index.html",
             self.context({
-                'on_going_campaign': on_going_campaign,
-                'on_going_campaign_vote': on_going_campaign_vote
+
             })
         )
