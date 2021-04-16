640444849c17988f76ee5ccf65a01adee18fbc1d
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-16 16:06:37 +0200

    - Mise en place du modèle User et UserMembership - Configuration de la section admin de gestion des utilisateurs

--- cla_auth/admin.py
+++ cla_auth/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.shortcuts import resolve_url
+from django.utils.html import mark_safe, escape
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.utils import timezone
 
-# Register your models here.
+from .models import *
+
+
+# Remove default User management interface
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+
+    class UserInfosInline(admin.StackedInline):
+        model = UserInfos
+        fields = (
+            'email_school',
+            'phone',
+            ('promo', 'cursus'),
+            'birthdate',
+            ('activated_on', 'valid_until')
+        )
+        readonly_fields = 'activated_on', 'valid_until'
+        extra = 1
+        min_num = 1
+
+    class MembershipInline(admin.StackedInline):
+        model = UserMembership
+        fields = (
+                     ('amount', 'paid_on', 'paid_by'),
+                     ('refunded', 'refunded_amount', 'refunded_on')
+        )
+        extra = 0
+        min_num = 0
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    inlines = [
+        UserInfosInline,
+        MembershipInline,
+    ]
--- cla_auth/models.py
+++ cla_auth/models.py
@@ -1,17 +1,40 @@
 import re
+import os
+import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django_resized import ResizedImageField
+
 from cla_web.utils import current_school_year
+
+
+class FilePath:
+
+    @classmethod
+    def _path(cls, instance, pathlist, filename):
+        ext = filename.split('.')[-1]
+        filename = "%s-%s.%s" % (slugify(instance.name), uuid.uuid4(), ext)
+        pathlist.append(filename)
+        return os.path.join(*pathlist)
+
+    @classmethod
+    def picture(cls, instance, filename):
+        return cls._path(instance, ["cla_ath", "user", "picture"], filename)
+
 
 
 class UserInfos(models.Model):
     """
         The UserInfo model extends user by adding new properties
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="infos")
-    cursus = models.CharField(max_length=100, null=True)
-    promo = models.IntegerField()
+
+    class AccountType(models.TextChoices):
+        STUDENT = 'student', "Étudiant"
+        SCHOOL = 'school', 'Personnel de l\'école'
+        ADMIN = 'admin', 'Compte administrateur'
+        OTHER = 'other', 'Autre'
 
     class Colleges(models.TextChoices):
         G1 = 'g1', 'G1'
@@ -21,6 +44,89 @@ class UserInfos(models.Model):
         IE3 = 'ie3', 'IE3'
         IE4 = 'ie4', 'IE4'
         IE5 = 'ie5', 'IE5'
+        OTHER = 'other', 'Autre'
+
+    class CursusChoices(models.TextChoices):
+
+        # # # # # # # # # #
+        # CURSUS CENTRALE #
+        # # # # # # # # # #
+
+        G1 = "G1", "G1"
+        G1_DD_EDHEC = "G1-DD-EDHEC", "G1 en double diplôme avec EDHEC"
+        G1_DD_INTERNATIONAL = "G1-DD-INTERNATIONAL", "G1 en double diplôme international"
+        G1P = "G1'", "G1'"
+        G1P_DD_EDHEC = "G1'-DD-EDHEC", "G1' en double diplôme EDHEC"
+        G1P_DD_INTERNATIONAL = "G1'-DD-INTERNATIONAL", "G1' en double diplôme international"
+
+        G2 = "G2", "G2"
+        G2_DD_EDHEC = "G2-DD-EDHEC", "G2 en double diplôme avec l'EDHEC"
+        G2_DD_INTERNATIONAL = "G2-DD-INTERNATIONAL", "G2 en double diplôme international"
+        G2_CESURE_FEV = "G2-CESURE-FEV", "G2 en césure février/février"
+        G2_CESURE_SEPT = "G2-CESURE-SEPT", "G2 en année sabatique"
+
+        G2P = "G2'", "G2'"
+        G2P_DD_EDHEC = "G2'-DD-EDHEC", "G2' en double diplôme avec l'EDHEC"
+        G2P_DD_INTERNATIONAL = "G2'-DD-INTERNATIONAL", "G2' en double diplôme international"
+        G2P_CESURE_FEV = "G2'-CESURE-FEV", "G2 en césure février/février"
+        G2P_CESURE_SEPT = "G2'-CESURE-SEPT", "G2 en année sabatique"
+
+        G3 = "G3", "G3"
+        G3_GEC = "G3-GEC", "G3 en mobilité intercentrale"
+        G3_DD_EDHEC = "G3-DD-EDHEC", "Double diplôme avec l'EDHEC"
+        G3_DD_INTERNATIONAL = "DD-INTERNATIONAL", "Double diplôme internationnal"
+        G3_DD_FRANCE = "DD-FRANCE", "Double diplôme en France"
+
+        G3_DIPLOME_DD_EDHEC = "G3-DIPLOME-DD-EDHEC", "Diplomé"
+        G3_DIPLOME_DD_INTERNATIONAL = "G3-DIPLOME-DD-INTERNATIONAL", "Diplomé"
+
+        G3_DIPLOME = "G3-DIPLOME", "Diplomé"
+
+        # # # # # # # # #
+        # CURSUS  ITEEM #
+        # # # # # # # # #
+
+        IE1 = "IE1", "IE1"
+        IE1P = "IE1P", "IE1'"
+
+        IE2 = "IE2", "IE2"
+        IE2P = "IE2P", "IE2'"
+
+        IE3 = "IE3", "IE3"
+        IE3P = "IE3P", "IE3'"
+
+        IE4 = "IE4", "IE4"
+        IE4P = "IE4P", "IE4'"
+
+        IE5 = "IE5", "IE5"
+        IE5P = "IE5P", "IE5'"
+
+        IE5_DIPLOME = "IE5-DIPLOME"
+
+        # # # # # # # # #
+        # CURSUS  AUTRE #
+        # # # # # # # # #
+        AUTRE = "Autre", "Autre cursus"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="infos", to_field="username")
+    email_school = models.EmailField(null=False, verbose_name="Adresse mail de l'école")
+    account_type = models.CharField(max_length=10, choices=AccountType.choices)
+    phone = models.CharField(max_length=20, blank=True)
+    cursus = models.CharField(max_length=100, null=True, choices=CursusChoices.choices, verbose_name="Cursus")
+    promo = models.PositiveIntegerField(verbose_name="Promotion (lors de l'arrivée à Centrale)")
+    birthdate = models.DateField(verbose_name="Date de naissance")
+    activated_on = models.DateTimeField(verbose_name="Date d'activation du compte", null=True)
+    valid_until = models.DateTimeField(verbose_name="Compte valide jusqu'au", null=True)
+    picture = ResizedImageField(
+        verbose_name="Photo de profil",
+        storage=FilePath.picture,
+        size=[500, 500],
+        quality=90,
+        force_format="JPEG",
+        keep_meta=False,
+        null=True,
+        blank=True
+    )
 
     def is_from_centrale(self):
         return re.match(r'^G\d.*', self.cursus)
@@ -30,13 +136,174 @@ class UserInfos(models.Model):
 
     @property
     def college(self):
-        if self.promo <= current_school_year()+1:
+        if self.promo <= current_school_year() + 1:
             return self.Colleges.G3 if self.is_from_centrale() else self.Colleges.IE5
-        elif self.promo == current_school_year()+2:
+        elif self.promo == current_school_year() + 2:
             return self.Colleges.G2 if self.is_from_centrale() else self.Colleges.IE4
-        elif self.promo == current_school_year()+3:
+        elif self.promo == current_school_year() + 3:
             return self.Colleges.G1 if self.is_from_centrale() else self.Colleges.IE3
-        elif self.promo == current_school_year()+4:
+        elif self.promo == current_school_year() + 4:
             return self.Colleges.IE1_IE2
-        elif self.promo == current_school_year()+5:
+        elif self.promo == current_school_year() + 5:
             return self.Colleges.IE1_IE2
+
+    @property
+    def next_cursus_choices(self):
+        if self.cursus == self.CursusChoices.G1:
+            class NextCursusChoices(models.TextChoices):
+                G1P = self.CursusChoices.G1P
+                G2 = self.CursusChoices.G2
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G1_DD_EDHEC:
+            class NextCursusChoices(models.TextChoices):
+                G1P_DD_EDHEC = self.CursusChoices.G1P_DD_EDHEC
+                G2_DD_EDHEC = self.CursusChoices.G2_DD_EDHEC
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G1_DD_INTERNATIONAL:
+            class NextCursusChoices(models.TextChoices):
+                G1P_DD_INTERNATIONAL = self.CursusChoices.G1P_DD_INTERNATIONAL
+                G2_DD_INTERNATIONAL = self.CursusChoices.G2_DD_INTERNATIONAL
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G1P:
+            class NextCursusChoices(models.TextChoices):
+                G2 = self.CursusChoices.G2
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G1P_DD_EDHEC:
+            class NextCursusChoices(models.TextChoices):
+                G2_DD_EDHEC = self.CursusChoices.G2_DD_EDHEC
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G1P_DD_INTERNATIONAL:
+            class NextCursusChoices(models.TextChoices):
+                G1P_DD_INTERNATIONAL = self.CursusChoices.G1P_DD_INTERNATIONAL
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G2 or self.cursus == self.CursusChoices.G2P:
+            class NextCursusChoices(models.TextChoices):
+                G2P = self.CursusChoices.G2P
+                G3 = self.CursusChoices.G3
+                G3_DD_EDHEC = self.CursusChoices.G3_DD_EDHEC
+                G3_DD_INTERNATIONAL = self.CursusChoices.G3_DD_INTERNATIONAL
+                G2_CESURE_FEV = self.CursusChoices.G2_CESURE_FEV
+                G2_CESURE_SEPT = self.CursusChoices.G2_CESURE_SEPT
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G2_DD_EDHEC or self.cursus == self.CursusChoices.G2P_DD_EDHEC:
+            class NextCursusChoices(models.TextChoices):
+                G2P_DD_EDHEC = self.CursusChoices.G2P_DD_EDHEC
+                G3_DIPLOME_DD_EDHEC = self.CursusChoices.G3_DIPLOME_DD_EDHEC
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G2_DD_INTERNATIONAL or self.cursus == self.CursusChoices.G2P_DD_INTERNATIONAL:
+            class NextCursusChoices(models.TextChoices):
+                G2P_DD_INTERNATIONAL = self.CursusChoices.G2P_DD_INTERNATIONAL
+                G3_DIPLOME_DD_INTERNATIONAL = self.CursusChoices.G3_DIPLOME_DD_INTERNATIONAL
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G2_CESURE_FEV or self.CursusChoices.G2_CESURE_SEPT:
+            class NextCursusChoices(models.TextChoices):
+                G2P = self.CursusChoices.G2P
+                G3 = self.CursusChoices.G3
+                G3_DD_EDHEC = self.CursusChoices.G3_DD_EDHEC
+                G3_DD_INTERNATIONAL = self.CursusChoices.G3_DD_INTERNATIONAL
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G3:
+            class NextCursusChoices(models.TextChoices):
+                G3 = self.CursusChoices.G3
+                G3_DIPLOME = self.CursusChoices.G3_DIPLOME
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G3_DD_EDHEC:
+            class NextCursusChoices(models.TextChoices):
+                G3_DD_EDHEC = self.CursusChoices.G3_DD_EDHEC
+                G3_DIPLOME = self.CursusChoices.G3_DIPLOME
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G3_DD_INTERNATIONAL:
+            class NextCursusChoices(models.TextChoices):
+                G3_DD_INTERNATIONAL = self.CursusChoices.G3_DD_INTERNATIONAL
+                G3_DIPLOME = self.CursusChoices.G3_DIPLOME
+
+            return NextCursusChoices
+
+        elif self.cursus == self.CursusChoices.G3_DD_FRANCE:
+            class NextCursusChoices(models.TextChoices):
+                G3_DD_FRANCE = self.CursusChoices.G3_DD_FRANCE
+                G3_DIPLOME = self.CursusChoices.G3_DIPLOME
+
+            return NextCursusChoices
+
+        if self.cursus in {self.CursusChoices.IE1, self.CursusChoices.IE1P}:
+            class NextCursusChoices(models.TextChoices):
+                IE2 = self.CursusChoices.IE2
+                IE1P = self.CursusChoices.IE1P
+
+            return NextCursusChoices
+
+        elif self.cursus in {self.CursusChoices.IE2, self.CursusChoices.IE2P}:
+            class NextCursusChoices(models.TextChoices):
+                IE3 = self.CursusChoices.IE3
+                IE2P = self.CursusChoices.IE2P
+
+            return NextCursusChoices
+
+        elif self.cursus in {self.CursusChoices.IE3, self.CursusChoices.IE3P}:
+            class NextCursusChoices(models.TextChoices):
+                IE4 = self.CursusChoices.IE4
+                IE3P = self.CursusChoices.IE3P
+
+            return NextCursusChoices
+
+        elif self.cursus in {self.CursusChoices.IE4, self.CursusChoices.IE4P}:
+            class NextCursusChoices(models.TextChoices):
+                IE5 = self.CursusChoices.IE5
+                IE4P = self.CursusChoices.IE4P
+
+            return NextCursusChoices
+
+        elif self.cursus in {self.CursusChoices.IE5, self.CursusChoices.IE5}:
+            class NextCursusChoices(models.TextChoices):
+                IE5 = self.CursusChoices.IE5
+                IE5_DIPLOME = self.CursusChoices.IE5_DIPLOME
+
+            return NextCursusChoices
+
+        # Fallback next cursus choices
+        class NextCursusChoices(models.TextChoices):
+            AUTRE = self.CursusChoices.AUTRE
+
+        return NextCursusChoices
+
+
+class UserMembership(models.Model):
+    class MeanOfPayment(models.TextChoices):
+        PUMPKIN = 'Pumpkin', 'pumpkin'
+        CHECK = 'Chèque', 'check'
+        CASH = 'Liquide', 'cash'
+        TRANSFER = 'Virement', 'transfer',
+        CARD = 'Carte bancaire', 'card'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="membership", to_field="username")
+    amount = models.PositiveIntegerField(verbose_name="Montant de la cotisation")
+    paid_on = models.DateField(verbose_name="Date de paiement")
+    paid_by = models.CharField(max_length=100, choices=MeanOfPayment.choices, verbose_name="Moyen de paiement")
+    refunded = models.BooleanField(default=False, blank=True, verbose_name="La cotisation a été remboursée")
+    refunded_amount = models.PositiveIntegerField(null=True, blank=True, verbose_name="Montant remboursé")
+    refunded_on = models.DateField(null=True, blank=True, verbose_name="Date du remboursement")
