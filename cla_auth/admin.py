95c9a8e53906d4da950399c79acbf4d14b31632f
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-16 18:07:34 +0200

    Différents ajouts au niveau de la gestion utilisateur, modification de la logique de création

--- cla_auth/admin.py
+++ cla_auth/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 from django.shortcuts import resolve_url
 from django.utils.html import mark_safe, escape
 from django.template.loader import render_to_string
@@ -8,7 +9,7 @@ from django.conf import settings
 from django.utils import timezone
 
 from .models import *
-
+from cla_auth.forms.admin_user_form import UserCreationForm
 
 # Remove default User management interface
 admin.site.unregister(User)
@@ -16,31 +17,78 @@ admin.site.unregister(User)
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-
     class UserInfosInline(admin.StackedInline):
         model = UserInfos
+        verbose_name = "Informations complémentaires"
         fields = (
-            'email_school',
-            'phone',
+            ('email_school', 'phone'),
             ('promo', 'cursus'),
             'birthdate',
-            ('activated_on', 'valid_until')
+            ('activated_on', 'valid_until'),
+            'account_type'
         )
         readonly_fields = 'activated_on', 'valid_until'
         extra = 1
         min_num = 1
+        can_delete = False
 
     class MembershipInline(admin.StackedInline):
         model = UserMembership
         fields = (
-                     ('amount', 'paid_on', 'paid_by'),
-                     ('refunded', 'refunded_amount', 'refunded_on')
+            ('amount', 'paid_on', 'paid_by'),
+            ('refunded', 'refunded_amount', 'refunded_on')
         )
         extra = 0
         min_num = 0
 
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        (
+            _('Personal info'),
+            {
+                'fields': (('first_name', 'last_name'), 'email')
+            }
+        ),
+        (
+            "Information de connexion",
+            {
+                'fields': ('username', 'password', ('last_login', 'date_joined')),
+                'classes': ('collapse',),
+            }
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+                'classes': ('collapse',),
+            }
+        ),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (('first_name', 'last_name'), 'email'),
+        }),
+    )
+    readonly_fields = 'username', 'last_login', 'date_joined'
     inlines = [
         UserInfosInline,
         MembershipInline,
     ]
+    list_display = ('username', 'first_name', 'last_name', 'email_school', 'is_activated', 'is_validated')
+    list_filter = ('is_staff',)
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    add_form = UserCreationForm
+
+    def email_school(self, obj: User):
+        return obj.infos.email_school
+    email_school.short_description = 'Email école'
+
+    def is_activated(self, obj: User):
+        return obj.infos.activated_on is not None
+    is_activated.short_description = 'Compté activé'
+    is_activated.boolean = True
+
+    def is_validated(self, obj: User):
+        return obj.infos.valid_until is not None and obj.infos.valid_until > timezone.now()
+    is_validated.short_description = 'Compte validé'
+    is_validated.boolean = True
