7db23e5ebefe709f31e61ebad9ace8e7d6c9d0c8
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-17 21:55:05 +0200

    Enregistrement des signaux

--- cla_auth/apps.py
+++ cla_auth/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
-
 class ClaAuthConfig(AppConfig):
     name = 'cla_auth'
+
+    def ready(self):
+        # Enregistrement des fonctions de l'application liées aux signaux
+        from django.contrib.auth.models import User
+        from django.db.models.signals import post_save
+        from cla_auth.signals import send_activation_email
+
+        post_save.connect(send_activation_email, sender=User)  # Envoie d'un mail lors de la création d'un compte
