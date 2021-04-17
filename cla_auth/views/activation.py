898d487b1c2815eea2c1b88ec10f5ec9838e75d5
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-17 21:54:16 +0200

    Première implémentation simplissime de la page d'activation

--- cla_auth/views/activation.py
+++ cla_auth/views/activation.py
@@ -1,16 +1,21 @@
-
-import requests
+import jwt
 
 from django.views.generic import View
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404
 
 from cla_auth.models import UserInfos
 
 
 def activate(req, activation_jwt):
-    req.session['next'] = req.GET.get('next', reverse('cla_public:index'))
-
-
+    # Retrieve jwt payload to fetch user
+    jwt_payload = jwt.decode(activation_jwt, algorithms=["HS256"], options={"verify_signature": False})
+    user = get_object_or_404(User, pk=jwt_payload.get('pk'))
+    if hasattr(user, 'infos'):
+        # Check that the token is correct and that activation was not already done
+        if user.infos.check_activation_jwt(activation_jwt) and user.infos.activated_on is None:
+            return HttpResponse(content=f"{user.first_name} {user.last_name}")
+    raise Http404
