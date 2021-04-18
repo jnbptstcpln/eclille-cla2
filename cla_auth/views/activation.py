eeabd6a10fd33681e7120e5605187e32220fadf1
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-18 13:32:47 +0200

    Mise en place des différentes views lié à l'activation du compte

--- cla_auth/views/activation.py
+++ cla_auth/views/activation.py
@@ -10,12 +10,25 @@ from django.http import HttpResponse, Http404
 from cla_auth.models import UserInfos
 
 
-def activate(req, activation_jwt):
+def _get_user_from_jwt(activation_jwt):
     # Retrieve jwt payload to fetch user
     jwt_payload = jwt.decode(activation_jwt, algorithms=["HS256"], options={"verify_signature": False})
     user = get_object_or_404(User, pk=jwt_payload.get('pk'))
     if hasattr(user, 'infos'):
         # Check that the token is correct and that activation was not already done
         if user.infos.check_activation_jwt(activation_jwt) and user.infos.activated_on is None:
-            return HttpResponse(content=f"{user.first_name} {user.last_name}")
+            return user
     raise Http404
+
+
+def activate(req, activation_jwt):
+    user = _get_user_from_jwt(activation_jwt)
+    req.session.set('user_pk', user.pk)
+
+
+def activate_rgpd(req, activation_jwt):
+    user = _get_user_from_jwt(activation_jwt)
+
+
+def activate_password(req, activation_jwt):
+    user = _get_user_from_jwt(activation_jwt)
