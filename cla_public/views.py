5126e0bd26fdef37cb0ad49cdbffdfae9e6bec38
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-18 13:33:56 +0200

    Ajustement des fonctions index

--- cla_public/views.py
+++ cla_public/views.py
@@ -2,21 +2,11 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-class AbstractPublicView(View):
-
-    def context(self, context=dict()):
-        context['active_navigation'] = context.get('active_navigation', "accueil")
-        return context
-
-
-class IndexPublicView(AbstractPublicView):
-
-    def get(self, req):
-
-        return render(
-            req,
-            "cla_public/index.html",
-            self.context({
-
-            })
-        )
+def index(req):
+    return render(
+        req,
+        "cla_public/index.html",
+        {
+            'page_active': "home"
+        }
+    )
