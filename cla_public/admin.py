c6152b40b70faddc735a4c9742cbcfcf5e396297
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-19 22:43:15 +0200

    Mise en place d'un formulaire personnalisé pour les Flatpage dans la section administrateur

--- cla_public/admin.py
+++ cla_public/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages import admin as flatpage_admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from cla_public.forms import admin_flatpage
+
+# Define a new FlatPageAdmin
+admin.site.unregister(FlatPage)
+
+
+@admin.register(FlatPage)
+class FlatPageAdmin(flatpage_admin.FlatPageAdmin):
+    form = admin_flatpage.FlatpageForm
+    fieldsets = (
+        (None, {
+            'fields': ('url', 'title', 'content', 'sites'),
+        }),
+        (_('Advanced options'), {
+            'classes': ('collapse',),
+            'fields': (
+                'registration_required',
+                'template_name',
+            ),
+        }),
+    )
