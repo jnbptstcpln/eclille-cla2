743690f0f5e67d71b47ccd94d55a987adfd8286c
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-22 23:34:56 +0200

    Ajout final de la validation aux urls

--- cla_auth/urls.py
+++ cla_auth/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # Account activation
     path('activation/<str:activation_jwt>', views.activation.activate, name="activate"),
     path('activation/<str:activation_jwt>/rgpd', views.activation.activate_rgpd, name="activate_rgpd"),
+    path('activation/<str:activation_jwt>/motdepasse', views.activation.activate_password, name="activate_password"),
 
     # Account validation
     path('validation', views.validation.validate, name="validate"),
