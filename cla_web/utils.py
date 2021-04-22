787976dceb070bc82f7d33d044837ea1ce1bc678
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-22 11:50:20 +0200

    Ajout d'un générateur aléatoire de nombre à 6 chiffres

--- cla_web/utils.py
+++ cla_web/utils.py
@@ -1,7 +1,11 @@
 from django.utils import timezone
+import random
 
 
 def current_school_year():
     now = timezone.now()
     return now.year - 1 if 1 <= now.month < 9 else now.year
 
+
+def random_six_digits():
+    return random.randint(111111, 999999)
