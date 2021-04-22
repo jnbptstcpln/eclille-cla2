eb84bb2cf0dae8ce25b0a8f701aa12a8efedb4b4
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-22 23:34:37 +0200

    Implémentation de l'activation des comptes

---
+++ cla_auth/forms/activation.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class ActivationRgpdForm(forms.Form):
+
+    rgpd_agreement = forms.BooleanField(
+        label="J\'accepte que mes informations soient utilisées par Centrale Lille Associations tel que décrit et détaillé dans la charte de confidentialité.",
+        required=True,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
+
+class ActivationPasswordForm(forms.Form):
+
+    class Validators:
+
+        @classmethod
+        def validate_username(cls, value):
+            try:
+                user = User.objects.get(username=value)
+                if hasattr(user, 'infos'):
+                    if user.infos.activated_on is None:
+                        raise forms.ValidationError("Ce compte n'a pas encore été activé, veuillez consulter votre adresse mail en @xxxx.centralelille.fr")
+            except User.DoesNotExist:
+                pass
+
+    username = forms.CharField(
+        label="Identifiant de connexion",
+        help_text="Retenez le bien, c'est cet identifiant qui vous permettra de vous connecter",
+        required=True,
+        disabled=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prenom.nom'})
+    )
+    password1 = forms.CharField(
+        label="Votre mot de passe",
+        help_text="Entre 8 et 35 caractères",
+        required=True,
+        min_length=8,
+        max_length=35,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Mot de passe'}),
+    )
+    password2 = forms.CharField(
+        label="Confirmer votre mot de passe",
+        required=True,
+        min_length=8,
+        max_length=35,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirmer votre mot de passe'}),
+    )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 != password2:
+            raise forms.ValidationError('Les mots de passes ne correspondent pas')
+        return password2
--- cla_auth/views/activation.py
+++ cla_auth/views/activation.py
@@ -1,4 +1,5 @@
 import jwt
+import random
 
 from django.views.generic import View
 from django.shortcuts import render, redirect, reverse, get_object_or_404
@@ -6,8 +7,11 @@ from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
+from django.utils import timezone
 
 from cla_auth.models import UserInfos
+from cla_auth.forms.activation import ActivationRgpdForm, ActivationPasswordForm
+from cla_web.utils import current_school_year
 
 
 def _get_user_from_jwt(activation_jwt) -> User:
@@ -23,6 +27,10 @@ def _get_user_from_jwt(activation_jwt) -> User:
 
 def activate(req, activation_jwt):
     user = _get_user_from_jwt(activation_jwt)
+
+    # Reset activation related session's content
+    req.session['rgpd_agreement'] = False
+
     return render(
         req,
         'cla_auth/activation/activate.html',
@@ -35,6 +43,55 @@ def activate(req, activation_jwt):
 def activate_rgpd(req, activation_jwt):
     user = _get_user_from_jwt(activation_jwt)
 
+    if req.method == "POST":
+        form = ActivationRgpdForm(req.POST)
+        if form.is_valid():
+            req.session['rgpd_agreement'] = True
+            return redirect("cla_auth:activate_password", user.infos.activation_jwt)
+    else:
+        form = ActivationRgpdForm()
+
+    return render(
+        req,
+        'cla_auth/activation/activate_rgpd.html',
+        {
+            'form': form,
+            'user': user
+        }
+    )
+
 
 def activate_password(req, activation_jwt):
     user = _get_user_from_jwt(activation_jwt)
+
+    if not req.session.get('rgpd_agreement', False):
+        return redirect("cla_auth:activate_rgpd", user.infos.activation_jwt)
+
+    if req.method == "POST":
+        form = ActivationPasswordForm(req.POST, initial={'username': user.username})
+        if form.is_valid():
+            user.set_password(form.cleaned_data['password1'])  # Setting user password
+            user.save()
+
+            user.infos.activated_on = timezone.now()  # Saving the activation
+            user.infos.valid_until = timezone.datetime(year=current_school_year()+1, month=9, day=random.randint(10, 25))  # Setting the validation
+            user.infos.save()
+
+            return render(
+                req,
+                "cla_auth/activation/activate_success.html",
+                {
+                    'user': user
+                }
+            )
+    else:
+        form = ActivationPasswordForm(initial={'username': user.username})
+
+    return render(
+        req,
+        'cla_auth/activation/activate_password.html',
+        {
+            'form': form,
+            'user': user
+        }
+    )
