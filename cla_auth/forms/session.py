e730188ba6f6859d14435d1ef36ff04460ef97f6
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-22 09:56:40 +0200

    Ajout d'une vérification sur la validation du compte

--- cla_auth/forms/session.py
+++ cla_auth/forms/session.py
@@ -1,12 +1,26 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
 
+    class Validators:
+
+        @classmethod
+        def validate_username(cls, value):
+            try:
+                user = User.objects.get(username=value)
+                if hasattr(user, 'infos'):
+                    if user.infos.activated_on is None:
+                        raise forms.ValidationError("Ce compte n'a pas encore été activé, veuillez consulter votre adresse mail en @xxxx.centralelille.fr")
+            except User.DoesNotExist:
+                pass
+
     username = forms.CharField(
         label="Identifiant",
         required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prenom.nom'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prenom.nom'}),
+        validators=[Validators.validate_username]
     )
     password = forms.CharField(
         label="Mot de passe",
