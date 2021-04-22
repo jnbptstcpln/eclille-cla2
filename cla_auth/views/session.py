194d2d89f925f8a0b0167175f5a0baa9f41f929a
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-04-22 09:57:06 +0200

    Mise en place de la logique de connexion

--- cla_auth/views/session.py
+++ cla_auth/views/session.py
@@ -1,21 +1,34 @@
 
 import requests
 
-from django.views.generic import View
+from django.contrib import auth, messages
 from django.shortcuts import render, redirect, reverse
 from django.conf import settings
-from django.contrib import auth
 from django.contrib.auth.models import User
 
 from cla_auth.models import UserInfos
 from cla_auth.forms.session import LoginForm
+from django.utils import timezone
 
 
 def login(req):
-    req.session['next'] = req.GET.get('next', reverse('cla_public:index'))
+    req.session['next'] = req.GET.get('next', 'cla_public:index')
 
     if req.method == 'POST':
         form = LoginForm(req.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user: User = auth.authenticate(req, username=username, password=password)
+            if user is not None:
+                auth.login(req, user)
+                if hasattr(user, "infos"):
+                    # Check if the account lost its validation since last login
+                    if user.last_login <= user.infos.valid_until <= timezone.now():
+                        return redirect(req.session.get('next', 'cla_public:index'))
+                return redirect(req.session.get('next', 'cla_public:index'))
+            else:
+                form.add_error(None, "Combinaison identifiant/mot de passe incorrecte")
     else:
         form = LoginForm()
 
