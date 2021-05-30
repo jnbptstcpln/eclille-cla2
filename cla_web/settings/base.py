f3cb0008d75cd1e0817d1742357361ae49531744
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2021-05-30 18:25:32 +0200

    Ajout de la rétro compatibilité avec les mots de passe créé avec la fonction PHP password_hash (Algo bcrypt)

--- cla_web/settings/base.py
+++ cla_web/settings/base.py
@@ -115,6 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'cla_web.hashers.BCryptPasswordHasher',
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
