a5158a661ea7add8982cf26d9e2b6db38abfe932
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2022-09-01 11:14:29 +0200

    Log to a file

--- cla_web/settings/production.py
+++ cla_web/settings/production.py
@@ -70,3 +70,22 @@ AUTH_PASSWORD_VALIDATORS = [
 #        }
 #    }
 #}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
