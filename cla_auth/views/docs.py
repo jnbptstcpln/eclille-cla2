82d539daa6d1cab05fadd48438bf11b935dce002
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2024-01-24 23:15:05 +0100

    Fix to Cursus

--- cla_auth/views/docs.py
+++ cla_auth/views/docs.py
@@ -4,4 +4,4 @@ from cla_auth.models import *
 
 
 def cursus(req):
-    return JsonResponse(data={"cursus": UserInfos.Colleges.choices}, status=200)
+    return JsonResponse(data={"cursus": UserInfos.CursusChoices.choices}, status=200)
