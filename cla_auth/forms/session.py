c02962f85cf8a8f0b364a14af38b3bd76b40ac84
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2025-11-27 00:14:18 +0100

    Handle duplicate emails

--- cla_auth/forms/session.py
+++ cla_auth/forms/session.py
@@ -10,32 +10,35 @@ class LoginForm(forms.Form):
         def validate_username(cls, value):
             try:
                 user = User.objects.get(username=value)
-                if hasattr(user, 'infos'):
+                if hasattr(user, "infos"):
                     if user.infos.activated_on is None:
-                        raise forms.ValidationError("Ce compte n'a pas encore été activé, veuillez consulter votre adresse mail en @xxxx.centralelille.fr")
+                        raise forms.ValidationError(
+                            "Ce compte n'a pas encore été activé, veuillez consulter votre adresse mail en @xxxx.centralelille.fr"
+                        )
             except User.DoesNotExist:
                 pass
 
     username = forms.CharField(
         label="Identifiant",
         required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prenom.nom'}),
-        validators=[Validators.validate_username]
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "prenom.nom"}
+        ),
+        validators=[Validators.validate_username],
     )
     password = forms.CharField(
         label="Mot de passe",
         required=True,
-        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Mot de passe'})
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Mot de passe"}
+        ),
     )
     stay_logged_in = forms.BooleanField(
         label="Rester connecté",
         required=False,
-        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input"}),
     )
-    redirect = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput()
-    )
+    redirect = forms.CharField(required=False, widget=forms.HiddenInput())
 
 
 class ForgotForm(forms.Form):
@@ -44,14 +47,18 @@ class ForgotForm(forms.Form):
 
         @classmethod
         def validate_email(cls, value):
-            try:
-                user = User.objects.get(email=value)
-            except User.DoesNotExist:
-                raise forms.ValidationError("Aucun compte ne correspond à cette adresse email")
+            user = User.objects.filter(email=value).first()
+            if user is None:
+                raise forms.ValidationError(
+                    "Aucun compte ne correspond à cette adresse email"
+                )
+            return value
 
     email = forms.EmailField(
         label="Adresse mail personnelle",
         required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'prenom.nom@example.com'}),
-        validators=[Validators.validate_email]
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "prenom.nom@example.com"}
+        ),
+        validators=[Validators.validate_email],
     )
--- cla_auth/views/session.py
+++ cla_auth/views/session.py
@@ -18,100 +18,98 @@ from cla_web.notification import send_generic_email
 @csrf_protect
 def login(req):
     if req.user.is_authenticated:
-        return redirect(req.GET.get('next', 'cla_member:lobby'))
+        return redirect(req.GET.get("next", "cla_member:lobby"))
 
-    if req.method == 'POST':
+    if req.method == "POST":
         form = LoginForm(req.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            req.session['next'] = form.cleaned_data['redirect']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            req.session["next"] = form.cleaned_data["redirect"]
             user: User = auth.authenticate(req, username=username, password=password)
             if user is not None:
                 auth.login(req, user)
 
-                response = redirect(req.session.get('next', 'cla_member:lobby'))
+                response = redirect(req.session.get("next", "cla_member:lobby"))
 
                 if hasattr(user, "infos"):
                     # Check if the account lost its validation since last login
                     if user.infos.valid_until is not None:
-                        if user.last_login <= user.infos.valid_until <= timezone.now() and req.session.get('validation_alert', True):
+                        if (
+                            user.last_login <= user.infos.valid_until <= timezone.now()
+                            and req.session.get("validation_alert", True)
+                        ):
                             response = render(
                                 req,
                                 "cla_auth/validation/validate_alert_standalone.html",
                                 {
-                                    'redirect': resolve_url(req.session.get('next', 'cla_member:lobby'))
-                                }
+                                    "redirect": resolve_url(
+                                        req.session.get("next", "cla_member:lobby")
+                                    )
+                                },
                             )
 
                 # Handle stay_logged_in
-                if form.cleaned_data['stay_logged_in']:
-                    response.set_cookie('stay_logged_in', StayLoggedInMiddleware.get_jwt(user), expires=timezone.datetime.utcnow() + timezone.timedelta(days=30))
+                if form.cleaned_data["stay_logged_in"]:
+                    response.set_cookie(
+                        "stay_logged_in",
+                        StayLoggedInMiddleware.get_jwt(user),
+                        expires=timezone.datetime.utcnow()
+                        + timezone.timedelta(days=30),
+                    )
 
                 return response
 
             else:
                 form.add_error(None, "Combinaison identifiant/mot de passe incorrecte")
     else:
-        req.session['next'] = req.GET.get('next', 'cla_member:lobby')
-        req.session['validation_alert'] = req.GET.get('validation_alert', '1') == '1'
-        form = LoginForm(initial={'redirect': req.session['next']})
+        req.session["next"] = req.GET.get("next", "cla_member:lobby")
+        req.session["validation_alert"] = req.GET.get("validation_alert", "1") == "1"
+        form = LoginForm(initial={"redirect": req.session["next"]})
 
-    return render(
-        req,
-        'cla_auth/session/login.html',
-        {
-            'form': form
-        }
-    )
+    return render(req, "cla_auth/session/login.html", {"form": form})
 
 
 def forgot(req):
     if req.user.is_authenticated:
-        return redirect('cla_member:lobby')
+        return redirect("cla_member:lobby")
 
-    return render(
-        req,
-        'cla_auth/session/forgot.html'
-    )
+    return render(req, "cla_auth/session/forgot.html")
 
 
 def forgot_username(req):
     if req.user.is_authenticated:
-        return redirect('cla_member:lobby')
+        return redirect("cla_member:lobby")
 
     username = None
     server_error = False
     if req.method == "POST":
         form = ForgotForm(req.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
+            email = form.cleaned_data["email"]
             users = User.objects.filter(email=email)
             if users.count() == 1:
-                username = User.objects.get(email=email).username
+                user = users.first()
+                username = user.username
             elif users.count() > 1:
                 server_error = True
                 send_generic_email(
                     f"Plusieurs utilisateurs pour {email}",
-                    f"Lors de l'exécution de la fonction \"J'ai oublié mon identifiant\", l'adresse email {email} a retourné plusieurs comptes utilisateur. La personne concernée a été invitée à contacter cla@centralelille.fr et des actions doivent être prises pour préserver l'unicité utilisateur/email."
+                    f"Lors de l'exécution de la fonction \"J'ai oublié mon identifiant\", l'adresse email {email} a retourné plusieurs comptes utilisateur. La personne concernée a été invitée à contacter cla@centralelille.fr et des actions doivent être prises pour préserver l'unicité utilisateur/email.",
                 )
     else:
         form = ForgotForm()
 
     return render(
         req,
-        'cla_auth/session/forgot_username.html',
-        {
-            'username': username,
-            'form': form,
-            'server_error': server_error
-        }
+        "cla_auth/session/forgot_username.html",
+        {"username": username, "form": form, "server_error": server_error},
     )
 
 
 def forgot_password(req):
     if req.user.is_authenticated:
-        return redirect('cla_member:lobby')
+        return redirect("cla_member:lobby")
 
     error = None
     warning = None
@@ -121,25 +119,29 @@ def forgot_password(req):
     if req.method == "POST":
         form = ForgotForm(req.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-            user = User.objects.get(email=email)
+            email = form.cleaned_data["email"]
 
             users = User.objects.filter(email=email)
             if users.count() == 1:
-                reset_req: PasswordResetRequest = PasswordResetRequest.objects.get_or_create_reset_request(user=user)
-                if reset_req.attempt < 3:  # Send reset email if less than 3 attempts were made
+                user = users.first()
+                reset_req: PasswordResetRequest = (
+                    PasswordResetRequest.objects.get_or_create_reset_request(user=user)
+                )
+                if (
+                    reset_req.attempt < 3
+                ):  # Send reset email if less than 3 attempts were made
                     try:
                         send_mail(
-                            subject='[CLA] Réinitialiser votre mot de passe',
+                            subject="[CLA] Réinitialiser votre mot de passe",
                             from_email=settings.EMAIL_HOST_FROM,
                             recipient_list=[user.email],
                             message="Réinitialiser le mot de passe de votre compte CLA",
                             html_message=render_to_string(
-                                'cla_auth/reset/mail.html',
+                                "cla_auth/reset/mail.html",
                                 {
-                                    'site_href': f"https://{settings.ALLOWED_HOSTS[0]}",
-                                    'reset_href': f"https://{settings.ALLOWED_HOSTS[0]}{resolve_url('cla_auth:reset', reset_req.get_reset_jwt())}",
-                                }
+                                    "site_href": f"https://{settings.ALLOWED_HOSTS[0]}",
+                                    "reset_href": f"https://{settings.ALLOWED_HOSTS[0]}{resolve_url('cla_auth:reset', reset_req.get_reset_jwt())}",
+                                },
                             ),
                         )
                     except Exception as e:
@@ -157,7 +159,7 @@ def forgot_password(req):
                 server_error = True
                 send_generic_email(
                     f"Plusieurs utilisateurs pour {email}",
-                    f"Lors de l'exécution de la fonction \"J'ai oublié mon mot de passe\", l'adresse email {email} a retourné plusieurs comptes utilisateur. La personne concernée a été invitée à contacter cla@centralelille.fr et des actions doivent être prises pour préserver l'unicité utilisateur/email."
+                    f"Lors de l'exécution de la fonction \"J'ai oublié mon mot de passe\", l'adresse email {email} a retourné plusieurs comptes utilisateur. La personne concernée a été invitée à contacter cla@centralelille.fr et des actions doivent être prises pour préserver l'unicité utilisateur/email.",
                 )
 
     else:
@@ -165,14 +167,14 @@ def forgot_password(req):
 
     return render(
         req,
-        'cla_auth/session/forgot_password.html',
+        "cla_auth/session/forgot_password.html",
         {
-            'form': form,
-            'error': error,
-            'warning': warning,
-            'success': success,
-            'server_error': server_error
-        }
+            "form": form,
+            "error": error,
+            "warning": warning,
+            "success": success,
+            "server_error": server_error,
+        },
     )
 
 
