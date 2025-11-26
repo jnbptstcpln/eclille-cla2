a77f5487c0b0588a3280ada66ea04540c17d1e39
Jean-Baptiste Caplan <jnbptstcpln@gmail.com>
2025-11-27 00:07:32 +0100

    Handle optional pack field

--- cla_registration/views/admin.py
+++ cla_registration/views/admin.py
@@ -30,7 +30,11 @@ class RegistrationValidationView(UserPassesTestMixin, generic.FormView):
 
     def dispatch(self, request: HttpRequest, *args, **kwargs):
         # Get the registration
-        self.registration = get_object_or_404(Registration, pk=kwargs.pop("registration_pk", None), session__pk=kwargs.pop("session_pk", None))
+        self.registration = get_object_or_404(
+            Registration,
+            pk=kwargs.pop("registration_pk", None),
+            session__pk=kwargs.pop("session_pk", None),
+        )
         # Raise 404 error if the registration has already been linked to an account
         if self.registration.account is not None:
             raise Http404()
@@ -39,69 +43,81 @@ class RegistrationValidationView(UserPassesTestMixin, generic.FormView):
     def form_valid(self, form):
         try:
             # Update the registration
-            self.registration.first_name = form.cleaned_data['first_name']
-            self.registration.last_name = form.cleaned_data['last_name']
-            self.registration.email = form.cleaned_data['email']
-            self.registration.email_school = form.cleaned_data['email_school']
-            self.registration.birthdate = form.cleaned_data['birthdate']
-            self.registration.contribution = form.cleaned_data['amount']
-            self.registration.original_school = form.cleaned_data.get('original_school', None)
+            self.registration.first_name = form.cleaned_data["first_name"]
+            self.registration.last_name = form.cleaned_data["last_name"]
+            self.registration.email = form.cleaned_data["email"]
+            self.registration.email_school = form.cleaned_data["email_school"]
+            self.registration.birthdate = form.cleaned_data["birthdate"]
+            self.registration.contribution = form.cleaned_data["amount"]
+            self.registration.original_school = form.cleaned_data.get(
+                "original_school", None
+            )
             # If the student finally change his mind about taking the pack
             if self.registration.is_pack_available:
-                self.registration.pack = int(form.cleaned_data['pack'])
+                self.registration.pack = int(form.cleaned_data["pack"])
                 self.registration.type = f'{self.registration.type_prefix}_{"pack" if self.registration.pack else "cla"}'
                 if self.registration.pack:
                     self.registration.rgpd_sharing_alumni = True
         except Exception as e:
-            bugsnag.notify(e, metadata={'form': form.cleaned_data})
-        
+            bugsnag.notify(e, metadata={"form": form.cleaned_data})
+
         # Creating user object
         user = User(
-            username=create_username(form.cleaned_data['first_name'], form.cleaned_data['last_name']),
-            first_name=form.cleaned_data['first_name'],
-            last_name=form.cleaned_data['last_name'],
-            email=form.cleaned_data['email']
+            username=create_username(
+                form.cleaned_data["first_name"], form.cleaned_data["last_name"]
+            ),
+            first_name=form.cleaned_data["first_name"],
+            last_name=form.cleaned_data["last_name"],
+            email=form.cleaned_data["email"],
         )
         user.save()
 
         # Creating associated information
         user_infos = UserInfos(
             user=user,
-            email_school=form.cleaned_data['email_school'],
-            birthdate=form.cleaned_data['birthdate'],
-            promo=form.cleaned_data['promo'],
-            cursus=form.cleaned_data['cursus'],
-            phone=form.cleaned_data['phone'],
-            original_school=form.cleaned_data.get('original_school', None)
+            email_school=form.cleaned_data["email_school"],
+            birthdate=form.cleaned_data["birthdate"],
+            promo=form.cleaned_data["promo"],
+            cursus=form.cleaned_data["cursus"],
+            phone=form.cleaned_data["phone"],
+            original_school=form.cleaned_data.get("original_school", None),
         )
         user_infos.save()
 
         # Creating associated membership
         membership = UserMembership(
             user=user,
-            amount=form.cleaned_data['amount'],
-            paid_on=form.cleaned_data['paid_on'],
-            paid_by=form.cleaned_data['paid_by'],
-            paid_validated=form.cleaned_data['paid_validated'],
-            payment_installments=form.cleaned_data['payment_installments'],
-            payment_months=form.cleaned_data['payment_months'],
-            alumni_pack=form.cleaned_data['pack'] == 1,
+            amount=form.cleaned_data["amount"],
+            paid_on=form.cleaned_data["paid_on"],
+            paid_by=form.cleaned_data["paid_by"],
+            paid_validated=form.cleaned_data["paid_validated"],
+            payment_installments=form.cleaned_data["payment_installments"],
+            payment_months=form.cleaned_data["payment_months"],
+            alumni_pack=form.cleaned_data.get("pack") == 1,
         )
         membership.save()
 
         self.registration.account = user
         self.registration.save()
 
-        messages.success(self.request, f"L'utilisateur a été créé et un mail de bienvenue lui a été envoyé à {user.infos.email_school} avec un lien d'activation")
-        return redirect("admin:cla_registration_registrationsession_change", self.registration.session.pk)
-        #return redirect("admin:auth_user_change", user.pk)
+        messages.success(
+            self.request,
+            f"L'utilisateur a été créé et un mail de bienvenue lui a été envoyé à {user.infos.email_school} avec un lien d'activation",
+        )
+        return redirect(
+            "admin:cla_registration_registrationsession_change",
+            self.registration.session.pk,
+        )
+        # return redirect("admin:auth_user_change", user.pk)
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs.update({
-            'is_from_another_school': self.registration.original_school is not None,
-            'is_pack_available': self.registration.is_pack_available
-        })
+        kwargs.update(
+            {
+                "is_from_another_school": self.registration.original_school is not None,
+                "is_pack_available": self.registration.is_pack_available,
+            }
+        )
         return kwargs
 
     def get_initial(self):
@@ -136,18 +152,20 @@ class RegistrationValidationView(UserPassesTestMixin, generic.FormView):
             "amount": self.registration.contribution,
             "original_school": self.registration.original_school,
             "paid_on": timezone.now().date(),
-            "paid_by": None
+            "paid_by": None,
         }
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({
-            'opts': RegistrationSession._meta,
-            'original': self.registration,
-            'title': "Nouvel utilisateur",
-            'site_header': admin.site.site_header,
-            'site_title': admin.site.site_title
-        })
+        context.update(
+            {
+                "opts": RegistrationSession._meta,
+                "original": self.registration,
+                "title": "Nouvel utilisateur",
+                "site_header": admin.site.site_header,
+                "site_title": admin.site.site_title,
+            }
+        )
         return context
 
 
@@ -160,16 +178,29 @@ class RegistrationSessionExportView(UserPassesTestMixin, generic.View):
         session = get_object_or_404(RegistrationSession, pk=session_pk)
 
         response = HttpResponse(
-            content_type='text/csv',
-            headers={'Content-Disposition': 'attachment; filename="Campagne d\'adhésion ' + str(session.school_year) + '.csv"'},
+            content_type="text/csv",
+            headers={
+                "Content-Disposition": "attachment; filename=\"Campagne d'adhésion "
+                + str(session.school_year)
+                + '.csv"'
+            },
         )
 
         def get_mean_of_paiement(x: Registration):
             if x.account is not None:
                 if hasattr(x.account, "membership"):
                     output = x.account.membership.get_paid_by_display()
-                    output += f" - {x.account.membership.get_payment_installments_display()}" if x.account.membership.payment_installments is not None else ""
-                    output += f" {x.account.membership.get_payment_months_display()}" if x.account.membership.payment_months is not None and x.account.membership.payment_months >= 2 else ""
+                    output += (
+                        f" - {x.account.membership.get_payment_installments_display()}"
+                        if x.account.membership.payment_installments is not None
+                        else ""
+                    )
+                    output += (
+                        f" {x.account.membership.get_payment_months_display()}"
+                        if x.account.membership.payment_months is not None
+                        and x.account.membership.payment_months >= 2
+                        else ""
+                    )
                     return output
                 return "Aucune cotisation enregistrée"
             return "N'a pas encore payé"
@@ -189,26 +220,36 @@ class RegistrationSessionExportView(UserPassesTestMixin, generic.View):
             "Numéro de téléphone": lambda x: x.phone,
             "Date de naissance": lambda x: x.birthdate.strftime("%Y-%m-%d"),
             "Type": lambda x: x.type,
-            "Date inscription": lambda x: x.datetime_registration.strftime("%Y-%m-%d %H:%M"),
+            "Date inscription": lambda x: x.datetime_registration.strftime(
+                "%Y-%m-%d %H:%M"
+            ),
             "Ecole": lambda x: x.school,
-            "Double diplôme entrant": lambda x: f"Oui ({x.original_school})" if x.original_school is not None else "Non",
+            "Double diplôme entrant": lambda x: (
+                f"Oui ({x.original_school})" if x.original_school is not None else "Non"
+            ),
             "Montant de la cotisation": lambda x: f"{x.contribution}€",
             "A choisi le pack": lambda x: "Oui" if x.pack else "Non",
-            "A réglé sa cotisation": lambda x: "Oui" if x.account is not None else "Non",
+            "A réglé sa cotisation": lambda x: (
+                "Oui" if x.account is not None else "Non"
+            ),
             "Moyen de paiement": get_mean_of_paiement,
             "Paiement validé": get_paiement_validation,
-            "Référence adhésion": lambda x: x.pk
+            "Référence adhésion": lambda x: x.pk,
         }
 
         writer = csv.writer(response)
         writer.writerow([field for field in fields.keys()])
         for registration in session.registrations.order_by("last_name"):
-            writer.writerow([field_processing(registration) for field_processing in fields.values()])
+            writer.writerow(
+                [field_processing(registration) for field_processing in fields.values()]
+            )
 
         return response
 
 
-class MembershipProofView(WeasyTemplateResponseMixin, UserPassesTestMixin, generic.DetailView):
+class MembershipProofView(
+    WeasyTemplateResponseMixin, UserPassesTestMixin, generic.DetailView
+):
     model = User
     membership: UserMembership = None
     template_name = "cla_registration/admin/membership_pdf.html"
@@ -218,7 +259,7 @@ class MembershipProofView(WeasyTemplateResponseMixin, UserPassesTestMixin, gener
 
     def get_context_data(self, **kwargs):
 
-        if not hasattr(self.object, 'infos'):
+        if not hasattr(self.object, "infos"):
             raise Http404()
 
         if not self.object.infos.has_active_membership():
@@ -227,12 +268,14 @@ class MembershipProofView(WeasyTemplateResponseMixin, UserPassesTestMixin, gener
         self.membership = self.object.infos.get_active_membership()
 
         context = super().get_context_data(**kwargs)
-        context.update({
-            'user': self.object,
-            'membership': self.membership,
-            'treasurer': Association.objects.get_cla().members.filter(_role=AssociationMember.Roles.TREASURER).first(),
-            'associations': {
-                'cla': Association.objects.get_cla()
+        context.update(
+            {
+                "user": self.object,
+                "membership": self.membership,
+                "treasurer": Association.objects.get_cla()
+                .members.filter(_role=AssociationMember.Roles.TREASURER)
+                .first(),
+                "associations": {"cla": Association.objects.get_cla()},
             }
-        })
+        )
         return context
